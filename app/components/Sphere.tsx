334ddc898e562018b46a5fc46f9784644ca1746f
Ryan Lee <rzlee@usc.edu>
2023-10-24 23:16:04 -0700

    commented out sphere

--- app/components/Sphere.tsx
+++ app/components/Sphere.tsx
@@ -1,65 +1,65 @@
-'use client'
-import React, { useRef, useEffect } from 'react'
-import * as THREE from 'three'
+// 'use client'
+// import React, { useRef, useEffect } from 'react'
+// import * as THREE from 'three'
 
-const Sphere: React.FC = () => {
-    const containerRef = useRef<HTMLDivElement>(null)
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const scene = new THREE.Scene()
-            const camera = new THREE.PerspectiveCamera(
-                75,
-                window.innerWidth / window.innerHeight,
-                0.1,
-                1000
-            )
-            const renderer = new THREE.WebGLRenderer()
-            renderer.setSize(window.innerWidth / 1.5, window.innerHeight / 1.5)
-            renderer.setClearColor(0xffffff, 0)
-            containerRef.current?.appendChild(renderer.domElement)
-            camera.position.z = 2
+// const Sphere: React.FC = () => {
+//     const containerRef = useRef<HTMLDivElement>(null)
+//     useEffect(() => {
+//         if (typeof window !== 'undefined') {
+//             const scene = new THREE.Scene()
+//             const camera = new THREE.PerspectiveCamera(
+//                 75,
+//                 window.innerWidth / window.innerHeight,
+//                 0.1,
+//                 1000
+//             )
+//             const renderer = new THREE.WebGLRenderer()
+//             renderer.setSize(window.innerWidth / 1.5, window.innerHeight / 1.5)
+//             renderer.setClearColor(0xffffff, 0)
+//             containerRef.current?.appendChild(renderer.domElement)
+//             camera.position.z = 2
 
-            if (typeof window !== 'undefined') {
-                const geometry = new THREE.SphereGeometry()
-                const wireframe = new THREE.WireframeGeometry(geometry)
+//             if (typeof window !== 'undefined') {
+//                 const geometry = new THREE.SphereGeometry()
+//                 const wireframe = new THREE.WireframeGeometry(geometry)
 
-                const line = new THREE.LineSegments(wireframe)
-                line.material.depthTest = false
-                line.material.opacity = 0.25
-                line.material.color = new THREE.Color(0xffffff)
-                line.material.transparent = true
+//                 const line = new THREE.LineSegments(wireframe)
+//                 line.material.depthTest = false
+//                 line.material.opacity = 0.25
+//                 line.material.color = new THREE.Color(0xffffff)
+//                 line.material.transparent = true
 
-                scene.add(line)
+//                 scene.add(line)
 
-                const renderScene = () => {
-                    // line.rotation.x += 0.002
-                    line.rotation.y += 0.002
-                    renderer.render(scene, camera)
-                    requestAnimationFrame(renderScene)
-                }
+//                 const renderScene = () => {
+//                     // line.rotation.x += 0.002
+//                     line.rotation.y += 0.002
+//                     renderer.render(scene, camera)
+//                     requestAnimationFrame(renderScene)
+//                 }
 
-                // Call the renderScene function to start the animation loop
-                renderScene()
+//                 // Call the renderScene function to start the animation loop
+//                 renderScene()
 
-                const handleResize = () => {
-                    const width = window.innerWidth
-                    const height = window.innerHeight
+//                 const handleResize = () => {
+//                     const width = window.innerWidth
+//                     const height = window.innerHeight
 
-                    camera.aspect = width / height
-                    camera.updateProjectionMatrix()
+//                     camera.aspect = width / height
+//                     camera.updateProjectionMatrix()
 
-                    renderer.setSize(width, height)
-                }
+//                     renderer.setSize(width, height)
+//                 }
 
-                window.addEventListener('resize', handleResize)
+//                 window.addEventListener('resize', handleResize)
 
-                // Clean up the event listener when the component is unmounted
-                return () => {
-                    window.removeEventListener('resize', handleResize)
-                }
-            }
-        }
-    }, [])
-    return <div ref={containerRef} />
-}
-export default Sphere
+//                 // Clean up the event listener when the component is unmounted
+//                 return () => {
+//                     window.removeEventListener('resize', handleResize)
+//                 }
+//             }
+//         }
+//     }, [])
+//     return <div ref={containerRef} />
+// }
+// export default Sphere
