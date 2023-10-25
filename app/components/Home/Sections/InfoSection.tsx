35ac3b723642890551aaa3c93f7072dbe70b07bc
Ryan Lee <rzlee@usc.edu>
2023-10-24 22:49:43 -0700

    escape apostrophe

--- app/components/Home/Sections/InfoSection.tsx
+++ app/components/Home/Sections/InfoSection.tsx
@@ -1,47 +1,47 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 
-export default function InfoSection() {
-    return (
-        <section className="flex items-center h-screen w-full px-24 bg-dark">
-            <div
-                id="content"
-                className="flex justify-evenly items-center h-fit w-full"
-            >
-                <div className="text-white text-3xl font-light">
-                    <h1 className="mb-4">WORK →</h1>
-                    <h1 className="mb-4">ABOUT →</h1>
-                    <h1 className="mb-12">CONTACT →</h1>
-                    <button className="px-8 py-4 rounded-full text-dark text-xl bg-white">
-                        download cv↓
-                    </button>
-                </div>
-                <div className="text-white text-8xl font-light">
-                    <h1 className="mb-4">Let's Make</h1>
-                    <div className="flex items-center">
-                        <div>
-                            <h1 className="mb-4 font-bold">Great</h1>
-                            <h1>Things.</h1>
-                        </div>
-                        <div className="flex justify-center items-center ml-8 w-48 h-48 rounded-full bg-white">
-                            <h1 className="text-dark text-xl font-light">
-                                ryan lee ©
-                            </h1>
-                        </div>
-                    </div>
-                </div>
+// export default function InfoSection() {
+//     return (
+//         <section className="flex items-center h-screen w-full px-24 bg-dark">
+//             <div
+//                 id="content"
+//                 className="flex justify-evenly items-center h-fit w-full"
+//             >
+//                 <div className="text-white text-3xl font-light">
+//                     <h1 className="mb-4">WORK →</h1>
+//                     <h1 className="mb-4">ABOUT →</h1>
+//                     <h1 className="mb-12">CONTACT →</h1>
+//                     <button className="px-8 py-4 rounded-full text-dark text-xl bg-white">
+//                         download cv↓
+//                     </button>
+//                 </div>
+//                 <div className="text-white text-8xl font-light">
+//                     <h1 className="mb-4">Let's Make</h1>
+//                     <div className="flex items-center">
+//                         <div>
+//                             <h1 className="mb-4 font-bold">Great</h1>
+//                             <h1>Things.</h1>
+//                         </div>
+//                         <div className="flex justify-center items-center ml-8 w-48 h-48 rounded-full bg-white">
+//                             <h1 className="text-dark text-xl font-light">
+//                                 ryan lee ©
+//                             </h1>
+//                         </div>
+//                     </div>
+//                 </div>
 
-                <div className="absolute flex justify-between items-center bottom-0 w-full h-20 px-48 text-white border-solid border-t border-t-white">
-                    <Link href="https://www.github.com/ry-nl">
-                        <h1>GITHUB</h1>
-                    </Link>
-                    <Link href="https://www.linkedin.com/ry-nl">
-                        <h1>LINKEDIN</h1>
-                    </Link>
-                    <Link href="https://www.instagram.com/ry_nl">
-                        <h1>INSTAGRAM</h1>
-                    </Link>
-                </div>
-            </div>
-        </section>
-    )
-}
+//                 <div className="absolute flex justify-between items-center bottom-0 w-full h-20 px-48 text-white border-solid border-t border-t-white">
+//                     <Link href="https://www.github.com/ry-nl">
+//                         <h1>GITHUB</h1>
+//                     </Link>
+//                     <Link href="https://www.linkedin.com/ry-nl">
+//                         <h1>LINKEDIN</h1>
+//                     </Link>
+//                     <Link href="https://www.instagram.com/ry_nl">
+//                         <h1>INSTAGRAM</h1>
+//                     </Link>
+//                 </div>
+//             </div>
+//         </section>
+//     )
+// }
