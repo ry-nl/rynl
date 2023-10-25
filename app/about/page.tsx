0d9ff38860affa085ee970f0e9a707144ce80065
Ryan Lee <rzlee@usc.edu>
2023-10-24 22:32:21 -0700

    pages in progress note

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <main className="flex justify-center items-center w-full h-screen bg-dark text-4xl font-sans font-light text-white">
-            Page in Progress!
+            About page in Progress!
         </main>
     )
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <main className="flex justify-center items-center w-full h-screen bg-dark text-4xl font-sans font-light text-white">
-            Page in Progress!
+            Contact page in progress!
         </main>
     )
 }
