18d735da3c668496ae7405df2618c40caf8311a9
Ryan Lee <rzlee@usc.edu>
2023-11-19 17:00:35 -0800

    cv button fix, work page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,84 @@
+'use client'
+import { useState, useEffect } from 'react'
+
 export default function About() {
+    const [mousePosition, setMousePosition] = useState({
+        x: 0,
+        y: 0,
+    })
+    const [cursorVariant, setCursorVariant] = useState('defaultLight')
+
+    useEffect(() => {
+        const mouseMove = (e: any) => {
+            setMousePosition({
+                x: e.clientX,
+                y: e.clientY,
+            })
+        }
+
+        window.addEventListener('mousemove', mouseMove)
+
+        return () => {
+            window.removeEventListener('mousemove', mouseMove)
+        }
+    }, [])
+
+    const cursorVariants = {
+        defaultLight: {
+            x: mousePosition.x - 12,
+            y: mousePosition.y - 12,
+        },
+        defaultDark: {
+            x: mousePosition.x - 12,
+            y: mousePosition.y - 12,
+            backgroundColor: '#111111',
+        },
+        link: {
+            x: mousePosition.x - 16,
+            y: mousePosition.y - 16,
+            width: 32,
+            height: 32,
+            backgroundColor: '#2d86fa',
+        },
+    }
+
+    const smallCursorVariants = {
+        defaultLight: {
+            x: mousePosition.x - 8,
+            y: mousePosition.y - 8,
+        },
+        defaultDark: {
+            x: mousePosition.x - 8,
+            y: mousePosition.y - 8,
+            backgroundColor: '#111111',
+        },
+        link: {
+            x: mousePosition.x - 12,
+            y: mousePosition.y - 12,
+            width: 24,
+            height: 24,
+            backgroundColor: '#2d86fa',
+        },
+    }
     return (
         <main className="flex justify-center items-center w-full h-screen bg-dark text-4xl font-sans font-light text-white">
-            About page in Progress!
+            <div className="flex justify-evenly w-full h-full">
+                <div className="w-1/2"></div>
+                <div className="w-1/2 text-xl">
+                    Nice to meet you! My name is Ryan, I am a software engineer
+                    based in Los Angeles, CA. I have experience in both startup
+                    and large company settings, working with cutting-edge
+                    technologies and collaborating with talented engineers to
+                    create scalable and reliable end-to-end solutions. I have
+                    gained valuable experience in developing and deploying
+                    back-end services, designing intuitive front-end UIs, and
+                    optimizing software performance and security. I pride myself
+                    in prioritizing quality and developing with a user-first
+                    approach, and am always eager to explore new opportunities
+                    to grow as a software engineer. Thank you for stopping by my
+                    page!
+                </div>
+            </div>
         </main>
     )
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,131 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+
+import Project from '../components/Work/Project'
+import { ProjectData } from '../components/Work/ProjectData'
+import { SlideButtonDarkUp, SlideButtonLightRight } from '../components/Buttons'
+
 export default function Work() {
+    const [mousePosition, setMousePosition] = useState({
+        x: 0,
+        y: 0,
+    })
+    const [cursorVariant, setCursorVariant] = useState('defaultLight')
+
+    useEffect(() => {
+        const mouseMove = (e: any) => {
+            setMousePosition({
+                x: e.clientX,
+                y: e.clientY,
+            })
+        }
+
+        window.addEventListener('mousemove', mouseMove)
+
+        return () => {
+            window.removeEventListener('mousemove', mouseMove)
+        }
+    }, [])
+
+    const cursorVariants = {
+        defaultLight: {
+            x: mousePosition.x - 12,
+            y: mousePosition.y - 12,
+        },
+        defaultDark: {
+            x: mousePosition.x - 12,
+            y: mousePosition.y - 12,
+            backgroundColor: '#111111',
+        },
+        link: {
+            x: mousePosition.x - 16,
+            y: mousePosition.y - 16,
+            width: 32,
+            height: 32,
+            backgroundColor: '#2d86fa',
+        },
+    }
+
+    const smallCursorVariants = {
+        defaultLight: {
+            x: mousePosition.x - 8,
+            y: mousePosition.y - 8,
+        },
+        defaultDark: {
+            x: mousePosition.x - 8,
+            y: mousePosition.y - 8,
+            backgroundColor: '#111111',
+        },
+        link: {
+            x: mousePosition.x - 12,
+            y: mousePosition.y - 12,
+            width: 24,
+            height: 24,
+            backgroundColor: '#2d86fa',
+        },
+    }
     return (
-        <main className="flex justify-center items-center w-full h-screen bg-dark text-4xl font-sans font-light text-white">
-            Work page in progress!
+        <main className="bg-dark text-4xl font-sans font-light text-white">
+            <motion.div
+                className="cursor"
+                variants={cursorVariants}
+                animate={cursorVariant}
+                transition={{
+                    duration: 0,
+                }}
+            />
+            <motion.div
+                className="small-cursor flex justify-center items-center"
+                variants={smallCursorVariants}
+                animate={cursorVariant}
+                transition={{
+                    duration: 0.1,
+                    ease: 'easeOut',
+                }}
+            />
+
+            <div
+                id="work-header"
+                className="flex justify-between items-center px-12 py-4"
+            >
+                <h1 className="text-2xl">WORK</h1>
+            </div>
+            <div id="work-container">
+                {ProjectData.map((project, index) => (
+                    <Project
+                        order={index + 1}
+                        name={project.name}
+                        company={project.company}
+                        position={project.position}
+                        tools={project.tools}
+                        description={project.description}
+                        accomplishments={project.accomplishments}
+                        image={project.image}
+                    />
+                ))}
+            </div>
+            <div className="flex justify-center items-center w-full h-48 border-t">
+                <div
+                    id="more-work-button-container"
+                    className="flex justify-center w-full h-fit font-thin tracking-wide text-xl"
+                >
+                    <div
+                        onMouseEnter={() => {
+                            setCursorVariant('link')
+                        }}
+                        onMouseLeave={() => {
+                            setCursorVariant('defaultDark')
+                        }}
+                    >
+                        <SlideButtonDarkUp
+                            buttonText="MORE WORK"
+                            link="https://github.com/ry-nl"
+                            popupText="→"
+                        />
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
